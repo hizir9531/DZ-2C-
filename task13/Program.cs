304390fe825a3371e3f69a88e4520fe9a12c5653
hizir <l-n-pe@mail.ru>
2023-04-17 22:43:36 +0300

    added task10 and modified gitignore

---
+++ task13/Program.cs
@@ -0,0 +1,65 @@
+﻿
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+// Main();
+
+// void Main()
+// {
+// 	bool isWorking = true;
+
+// 	while (isWorking)
+// 	{
+// 		Console.WriteLine("Input task");
+
+// 		string task = Console.ReadLine();
+
+// 		switch (task)
+// 		{
+// 			case "exit": isWorking = false; break;
+// 			case "t1": Task1(); break;
+// 			default:
+// 				break;
+// 		}
+// 	}
+// }
+
+// void Task1()
+// {
+// 	int a = ReadInt("a");
+// 	int b = ReadInt("b");
+// 	int less = a;
+
+// 	if (IsMoreThen(a, b))
+// 	{
+// 		less = b;
+// 	}
+
+// 	Console.WriteLine(less);
+// }
+
+// int ReadInt(string argument)
+// {
+// 	int number;
+
+// 	Console.Write($"Input {argument}:");
+
+// 	while (!int.TryParse(Console.ReadLine() , out number))
+// 	{
+// 		Console.WriteLine("It's not a number!");
+// 	}
+
+// 	return number;
+// }
+
+// bool IsMoreThen(int a, int b)
+// {
+// 	return a > b;
+// }
